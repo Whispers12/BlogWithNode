a6d73ddc1e85676c2b88f661981e980ed8b072a2
FrolovAndrey <afrolov@leclick.ru>
2018-04-07 11:55:40 +0300

    add auth tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,6 @@
-const puppeteer = require("puppeteer");
+const Page = require("./helpers/page");
+
+let page;
 
 beforeEach(async () => {
   page = await Page.build();
@@ -7,4 +9,26 @@ beforeEach(async () => {
 
 afterEach(async () => {
   await page.close();
+});
+
+test("the header has the correct text", async () => {
+  const text = await page.getContentsOf("a.brand-logo");
+
+  expect(text).toEqual("Blogster");
+});
+
+test("clicking login starts oauth flow", async () => {
+  await page.click(".right a");
+
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test("When signed in, shows logout button", async () => {
+  await page.login();
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+  expect(text).toEqual("Logout");
 });
