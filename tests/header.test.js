f4c3e36a739edacbe05b5d6120d12dfdfa6365a5
FrolovAndrey <afrolov@leclick.ru>
2018-04-07 09:53:22 +0300

    initialize tests

---
+++ tests/header.test.js
@@ -0,0 +1,10 @@
+const puppeteer = require("puppeteer");
+
+beforeEach(async () => {
+  page = await Page.build();
+  await page.goto("http://localhost:3000");
+});
+
+afterEach(async () => {
+  await page.close();
+});
